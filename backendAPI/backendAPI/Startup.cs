9f487c5c3b766c7139af6bb08ec07c534d484f29
DAVIS MUDAU <nmudau@Retrorabbit.co.za>
2019-01-29 12:09:40 +0200

    Fixed the database initial setup for microsft dot net core page

--- backendAPI/backendAPI/Startup.cs
+++ backendAPI/backendAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using backendAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace backendAPI
 {
@@ -26,6 +28,10 @@ namespace backendAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<DbContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
