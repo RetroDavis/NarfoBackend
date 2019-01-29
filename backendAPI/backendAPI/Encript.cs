fabc4682d12ba00a72cd1eef4bc7449fc2238c04
DAVIS MUDAU <nmudau@Retrorabbit.co.za>
2019-01-29 14:25:24 +0200

    backend added waiting for connection string settings

---
+++ backendAPI/backendAPI/Encript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backendAPI
+{
+    public class Encript
+    {
+        public static string Encrypt_user(string pass)
+        {
+            MD5 newEN = new MD5CryptoServiceProvider();
+            newEN.ComputeHash(ASCIIEncoding.ASCII.GetBytes(pass));
+            byte[] byteresults = newEN.Hash;
+            StringBuilder strb = new StringBuilder();
+            for (int a = 0; a < byteresults.Length; a++)
+            {
+                strb.Append(byteresults[a].ToString("X2"));
+            }
+            return strb.ToString();
+        }
+    }
+}
