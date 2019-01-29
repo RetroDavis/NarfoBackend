832ba6a09f9587a7b94bdc28c5f6cdee4fef9f5c
Bongz12 <46738627+Bongz12@users.noreply.github.com>
2019-01-29 16:13:58 +0200

    Working Get and PUT

--- backendAPI/ControllerUnitTest/APItest.cs
+++ backendAPI/ControllerUnitTest/APItest.cs
@@ -20,7 +20,7 @@ namespace Tests
 
         [TestCase(1, "Jack","Zulu","Male","2000/01/01")]
 
-        public async Task GetAsync(int value,string name,string surname,string gender,string dob)
+        public async Task Get(int value,string name,string surname,string gender,string dob)
         {
 
             //arrange
@@ -30,11 +30,11 @@ namespace Tests
             var response = await _client.GetAsync($"/get/{value}");
             // Check if status code is OK
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-            // Get JSON  of the hero from response
+            // Get JSON  of the worker from response
             var jsonResponse = await response.Content.ReadAsStringAsync();
-            // Deserialize response JSON to Hero class
+            // Deserialize response JSON to Employee class
             var EmployeeResponse = JsonConvert.DeserializeObject<Employee>(jsonResponse);
-            // Check if the hero is the same
+            // Check if the employee is the same
             Assert.AreEqual(worker.EmployeeID, EmployeeResponse.EmployeeID);
             Assert.AreEqual(worker.Name, EmployeeResponse.Name);
             Assert.AreEqual(worker.Surname, EmployeeResponse.Surname);
@@ -42,9 +42,39 @@ namespace Tests
             Assert.AreEqual(worker.DOB, EmployeeResponse.DOB);
 
         }
-       
 
-      
+        [TestCase(1, "Jacky", "Zulu", "Female", "2000/01/01")]
+
+        public async Task Put(int value, string name, string surname, string gender, string dob)
+        {
+
+            //arrange
+            //act
+            //asse
+            var worker = new Employee { EmployeeID = value, Name = name, Surname = surname, Gender = gender, DOB = dob };
+
+            // var response = await _client.GetAsync($"/get/{value}");
+            var stringContent = new StringContent(worker.ToString());        
+            var response = await _client.PutAsync($"/get/{value}", stringContent);
+
+            // Check if status code is OK
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            // Get JSON  of the worker from response
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            // Deserialize response JSON to Empoyee class
+            var EmployeeResponse = JsonConvert.DeserializeObject<Employee>(jsonResponse);
+            // Check if the Employee is the same
+
+            Assert.AreEqual(worker.EmployeeID, EmployeeResponse.EmployeeID);
+            Assert.AreEqual(worker.Name, EmployeeResponse.Name);
+            Assert.AreEqual(worker.Surname, EmployeeResponse.Surname);
+            Assert.AreEqual(worker.Gender, EmployeeResponse.Gender);
+            Assert.AreEqual(worker.DOB, EmployeeResponse.DOB);
+
+        }
+
+
+
 
 
     }
