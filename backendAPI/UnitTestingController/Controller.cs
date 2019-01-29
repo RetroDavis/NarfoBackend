8bbea098b87a3c08f9a39288759172b04210b267
Bongz12 <46738627+Bongz12@users.noreply.github.com>
2019-01-29 12:48:41 +0200

    test unit for the controller

---
+++ backendAPI/UnitTestingController/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingController
+{
+    class Controller
+    {
+        private EmployeeController econtroller;
+   
+        [TestCase(1, "just a name")]
+     
+        public void Get(int value,string outcome)
+        {
+
+            //arrange
+            //act
+            //assert
+           
+            var x = econtroller.Get(value);
+            Assert.AreEqual(outcome, x);
+        }
+
+
+       
+
+
+    }
+}
