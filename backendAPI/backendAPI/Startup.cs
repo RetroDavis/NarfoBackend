dab5c30cecdff5caa835eb3547a040237950aabe
soginga <ssoginga@retrorabbit.co.za>
2019-01-29 14:17:39 +0200

    prepared connection variable

--- backendAPI/backendAPI/Startup.cs
+++ backendAPI/backendAPI/Startup.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using backendAPI.Models;
+
 
 namespace backendAPI
 {
     public class Startup
     {
+        private string connection_string = @"Server=sql101labs1793591179000.westeurope.cloudapp.azure.com;Database=sql101.simtembile.soginga;User Id=simtembilesoginga;Password=Printwork31320;";
+
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,22 +32,26 @@ namespace backendAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+          
+            var connection = ;
+            services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(connection));
+
+            services.AddSingleton(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+          
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
+            
+
             app.UseMvc();
         }
     }
