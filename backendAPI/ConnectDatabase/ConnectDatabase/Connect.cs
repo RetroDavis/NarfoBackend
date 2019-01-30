bea1707925e27fa3c0edb9d331d0b122b92341b2
ThandiTQ <ttshabalala@retrorabbit.co.za>
2019-01-30 17:03:24 +0200

    Added tests to check database connection

---
+++ backendAPI/ConnectDatabase/ConnectDatabase/Connect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionToDatabase
+{
+    public class Connect
+    {
+
+        public string OpenConnection()
+        {
+            string connectionString;
+            SqlConnection connect;
+
+            connectionString = @"Data Source=dev.retrotest.co.za;Initial Catalog=narfo;User ID=group2;Password=jtn8TVNQMW_28esy";
+            connect = new SqlConnection(connectionString);
+            connect.Open();
+            bool status = false;
+
+            while (!status)
+            {
+                return "Open";
+            }
+
+            connect.Close();
+            return "Closed";
+
+
+        }
+
+        public string CloseConnection()
+        {
+
+            string connectionString;
+            SqlConnection connect;
+
+            connectionString = @"Data Source=dev.retrotest.co.za;Initial Catalog=narfo;User ID=group2;Password=jtn8TVNQMW_28esy";
+            connect = new SqlConnection(connectionString);
+            connect.Open();
+            connect.Close();
+            return "Closed";
+
+        }
+    }
+}
