9f53aae09c35b85f190091b35bdfa13fc3cac104
ThandiTQ <ttshabalala@retrorabbit.co.za>
2019-01-31 14:57:01 +0200

    Fixed API solution regarding unit tests

---
+++ ConnectionToDatabase/ConnectionToDatabase.Test/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionToDatabase.Test
+{
+    public class Tests
+    {
+        private Connection connect;
+
+        public Tests()
+        {
+            connect = new Connection();
+        }
+
+        [TestCase]
+        public void DatabaseConnection()
+        {
+            //assign
+            var Opentest = connect.DBConnection();
+
+            //act
+            //assert
+            Assert.AreEqual("Open", Opentest);
+
+        }
+    }
+}
---
+++ ConnectionToDatabase/ConnectionToDatabase/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionToDatabase
+{
+    public class Connection
+    {
+
+        public string DBConnection()
+        {
+            string connectionString;
+            SqlConnection connect;
+
+            connectionString = @"Data Source=dev.retrotest.co.za;Initial Catalog=narfo;User ID=group2;Password=jtn8TVNQMW_28esy";
+            connect = new SqlConnection(connectionString);
+            connect.Open();
+            bool status = false;
+
+            while (!status)
+            {
+                return "Open";
+            }
+            connect.Close();
+            return "Closed";
+        }
+    }
+}
